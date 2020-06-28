e2ff3fcb035d60290cca50e5ff30daef8957f04f
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-28 17:42:21 -0300

    add routes to app component

---
+++ src/pages/Poker/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Poker: React.FC = () => {
+  return <div>This is my poker page</div>;
+};
+
+export default Poker;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Poker from "../pages/Poker";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home}></Route>
+      <Route path="/poker" component={Poker}></Route>
     </Switch>
   );
 };
